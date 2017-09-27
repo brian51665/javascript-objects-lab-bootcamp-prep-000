ed6a7bc57528baa00cbec8e43b375f3125988844
brian51665 <bdavis51892@yahoo.com>
2017-09-27 16:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {sandwich:"peanut butter, jelly, bread", soup:"water, salt, chicken"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object)
-  return newObject
+  return Object.assign({}, object, [key:value])
 }
