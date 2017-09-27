e136a7791d995e72a4287a1f7211d661a9bcfd0b
brian51665 <bdavis51892@yahoo.com>
2017-09-27 16:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {sandwich:"peanut butter, jelly, bread", soup:"water, salt, chicken"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key]=value
-  return object
+  var newObject = object.assing({}, object)
+  return newObject
 }
